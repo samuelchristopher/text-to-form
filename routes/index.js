a7c5a216511d61de5db7b3289ccc5eed7bc282fb
Samuel Christopher <thesamchris@gmail.com>
2015-10-02 17:50:17 +0800

    Working on the text to input thing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var array = fs.readFileSync('./files/test.txt').toString().split("\n");
+  var line = array[1];
+  var words = line.split(" ");
+  var type = words[1].toLowerCase();
+  var max = 0;
+  if (type = 'float') {
+    var type = 'number';
+    var maximum = words[2];
+  }
+
+  res.render('index', {words, type, maximum});
 });
+
+
 
 module.exports = router;
